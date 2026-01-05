8dc9c613185625dcfedf30b866bf31c3d22c7552
Your Name <castelinolio11@gmail.com>
2026-01-05 23:10:23 +0530

    Updated notes

--- open_google.py
+++ open_google.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright
+import time
 
 
 def main():
@@ -8,8 +9,29 @@ def main():
         page.goto("https://www.google.com")
         title = page.title()
         print(title)
+        
+        input("Press Enter to close the browser...")
         browser.close()
 
 
 if __name__ == "__main__":
     main()
+    
+    
+    
+ ## code explanation 
+#  sync_playwright is a tool useed to control browser in synchronous way.
+ 
+#  browser = p.chromium.launch(headless=False) 
+#  the above line launches a chromium crowsere in non headless mode (visible mode)
+ 
+#  page = browser.new_page() 
+#  the above line creates a new page in the browser
+ 
+#  page.goto("https://www.google.com")
+#  the above line navigates to google home page
+ 
+#  title = page.title()     
+#  the above line gets the title of the current page
+ 
+#  then prints the title and closes the browser.
