4d2cf22404e47b3e7be2fb929dfc54e332b04add
Your Name <castelinolio11@gmail.com>
2026-01-07 23:52:50 +0530

    Initial commit

---
+++ test_sample.py
@@ -0,0 +1,13 @@
+#  below to understand the failing test case
+def test_example_fail():
+    
+    assert 1 + 1 == 4
+ 
+ #  below to understand the pass test case   
+def test_example_pass():
+    assert 1 + 1 == 2
+    
+    
+    
+    
+    
